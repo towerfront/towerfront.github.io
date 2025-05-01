5587e86d61e662a40803504c1e7e210750fa4c9b
robertsinfosec <490545+robertsinfosec@users.noreply.github.com>
2025-05-01 01:04:14 -0400

    Add Vite configuration and GitHub Actions workflow for build and deployment

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Base path will be root domain when deployed to towerfront.github.io
 })
